a8477d12afcbbcf2a3ac11f7ca8d374163cf0379
Christopher Demaria <cdemaria@navisite.com>
2017-02-03 15:24:17 -0500

    initial commit

---
+++ angular-client/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+
+import {MdCardModule} from '@angular2-material/card';
+import {MdButtonModule} from '@angular2-material/button';
+import {MdIconModule} from '@angular2-material/icon';
+import {MdIconRegistry} from '@angular2-material/icon';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MdCardModule,
+    MdButtonModule,
+    MdIconModule
+  ],
+  providers: [MdIconRegistry],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
